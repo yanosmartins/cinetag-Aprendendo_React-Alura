05cce737a4367d6703d741e936d33a2d5a2865bf
Yan Oliveira <yan.o.s.martins@gmail.com>
2024-01-03 15:27:58 -0300

    instalacao do react-routes-dom

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,6 @@
 function Inicio() {
-
     return(
         <h1>Olá mundo! Estou em outra página agora.</h1>
-
     )
 }
 export default Inicio;
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,3 @@
-// import Inicio from "pages/Inicio"
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-// function AppRoutes() {
-//     return(
-//         <BrowserRouter> //aqui ele está avisando que vão ter algumas rotas aqui dentro
-//         <Routes> //aqui ele funciona como um roteador. Ele quem é responsável pela troca de rotas
-//             <Route path="/" element={<Inicio />}></Route> //essa é a página inicial. Portanto n tem rota aqui.
-//         </Routes>
-//         </BrowserRouter>
-//     )
-// }
-
-// export default AppRoutes
-
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
